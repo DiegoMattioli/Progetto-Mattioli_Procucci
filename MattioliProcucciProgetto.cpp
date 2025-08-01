b78187ba54631b350d804c391c3b3d65c80025d6
Matteo Procucci <matteo.procucci@studio.unibo.it>
2025-08-01 10:10:06 +0200

    ho aggiunto la funzione che aggiorna la posizione dei boid dopo n secondi

--- MattioliProcucciProgetto.cpp
+++ MattioliProcucciProgetto.cpp
@@ -19,6 +19,7 @@ class Flock
 
     public:
     void add(int n)
+    //function adds n boids to the flock, their position and velocity are randomly generated
     {
         std::default_random_engine eng;
         std::uniform_real_distribution<double> position(0.,100.);
@@ -34,10 +35,48 @@ class Flock
             flock_.push_back(boid);
         }
     }
+
+    void add(Boid b)
+    //function that adds a single boid to the flock
+    {
+        flock_.push_back(b);
+    }
     
     int size()
     {
         return flock_.size();
+    }
+
+    void update_position(int refresh_rate)
+    //function that updates the position of all boids after: refresh_rate seconds 
+    {
+        for (auto it = flock_.begin(), last = flock_.end(); it < last; it++)
+        {
+            (*it).x += refresh_rate * (*it).vx;
+            (*it).y += refresh_rate * (*it).vy;
+        }
+        
+    }
+
+    double get_positionx(int i)
+    //function returns the x coordinate of the i th oid in the flock
+    {
+        auto it = (flock_.begin() + i);
+        if (it < flock_.end())
+        {
+            return (*it).x;
+        }
+        else{return -1.;}
+    }
+    double get_positiony(int i)
+    //function returns the x coordinate of the i th oid in the flock
+    {
+        auto it = (flock_.begin() + i);
+        if (it < flock_.end())
+        {
+            return (*it).y;
+        }
+        else{return -1.;}
     }
 };
 
@@ -49,5 +88,22 @@ TEST_CASE("Testing the Flock class")
     {
         f.add(10);
         CHECK(f.size() == 10);
+        Boid b{0,0,2,2};
+        f.add(b);
+        CHECK(f.size() == 11);
+    }
+
+    SUBCASE("testing the update_position function")
+    {
+        Boid b{0,0,2,2};
+        Boid b1{1,1,3,4};
+        f.add(b);
+        f.add(b1);
+        f.update_position(1);
+        CHECK(f.get_positionx(0) == 2.);
+        CHECK(f.get_positiony(0) == 2.);
+        CHECK(f.get_positionx(1) == 4.);
+        CHECK(f.get_positiony(1) == 5.);
+        CHECK(f.get_positionx(3) == -1.);
     }
 }
