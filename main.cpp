c1fdd1b1d9bfdfb472c48f856898a446d3b1ac51
Matteo Procucci <matteo.procucci@studio.unibo.it>
2025-08-11 18:49:11 +0200

    cambiato il main

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ int main() {
   try {
     double d, ds, a, c, s;
     int n, rip;
-    mp::Flock f{};
     std::cout << "Insert the number of Boids";
     std::cin >> n;
     if (n < 0) {
@@ -15,7 +14,6 @@ int main() {
           "Inserted an invalid value for the number of boids"};
     }
 
-    f.add(n);
     std::cout << "Select the value of the parameter d";
     std::cin >> d;
     std::cout << "Select the value of the parameter ds";
@@ -26,16 +24,18 @@ int main() {
     std::cin >> c;
     std::cout << "Select the value of the parameter s";
     std::cin >> s;
-    if ((d < 0 && d > 1) || (ds < 0 && ds > 1) || (a < 0 && a > 1) ||
-        (c < 0 && c > 1) || (s < 0 && s > 1)) {
-      throw std::runtime_error{
-          "Inserted an invalid value for one of the parameters, they have to "
-          "be between 0 and 1"};
+    if ((d < 0 && d > 1) || (ds < 0 && ds > 1) || (a < 0 && a > 1) ||(c < 0 && c > 1) || (s < 0 && s > 1))
+    {
+      throw std::runtime_error
+      {"\nInserted an invalid value for one of the parameters, all parameters must \n be between 0 and 1"};
     }
-    while (true) {
-      f.update_velocity(d, ds, s, a, c);
-      f.update_position(1);
+    else
+    {
+        mp::Parameters p{d, ds, s, a, c};
+        mp::Flock f{p};
+        f.add(n);
     }
+    
  } 
  catch (std::exception const& e) {
     std::cerr << "Caught exception: '" << e.what() << "'\n";
